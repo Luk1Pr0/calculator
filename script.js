e1f15f208dd707fd6a509a5a396597e124fb6728
Luk1Pr0 <lpytel16@gmail.com>
2020-12-05 19:25:16 +0100

    Display reset and decimal point functionality added

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear-button");
 
 const sendNumberValue = (num) => {
-    // If current display value is 0, then replace with entered numbers, else only add number to display
+    // If current display value is 0, then replace with entered numbers, else only add numbers to display
     const displayValue = calculatorDisplay.textContent;
     calculatorDisplay.textContent = displayValue === "0" ? num : displayValue + num;
+}
+
+const addDecimal = () => {
+    // If no decimal, then add only 1
+    if (!calculatorDisplay.textContent.includes(".")) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
 }
 
 // Add event listeners for numbers, operators and decimal point buttons
@@ -15,6 +22,14 @@ inputBtns.forEach(btn => {
     } else if (btn.classList.contains("operator")) {
         btn.addEventListener("click", () => sendNumberValue(btn.value));
     } else if (btn.classList.contains("decimal")) {
-        btn.addEventListener("click", () => sendNumberValue(btn.value));
+        btn.addEventListener("click", () => addDecimal());
     }
 })
+
+// Reset display
+const resetAll = () => {
+    calculatorDisplay.textContent = "0";
+}
+
+// Event listener
+clearBtn.addEventListener("click", resetAll); 
